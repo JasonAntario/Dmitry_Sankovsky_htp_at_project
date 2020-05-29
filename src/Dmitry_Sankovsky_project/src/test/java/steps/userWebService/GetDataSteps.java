2671d81a587c76ce15e4011b7efcc9c315e1fa40
JasonAntario <jasonantario@gmail.com>
2020-05-29 20:09:45 +0300

    Web service tests was changed

--- src/Dmitry_Sankovsky_project/src/test/java/steps/userWebService/GetDataSteps.java
+++ src/Dmitry_Sankovsky_project/src/test/java/steps/userWebService/GetDataSteps.java
@@ -2,49 +2,28 @@ package steps.userWebService;
 
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import utills.RequiredValues;
 import utills.Search;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class GetDataSteps {
-    private static String JSON = "src\\test\\java\\resources\\testdata\\search.json";
-    private static Search[] searches;
 
-    public static Search getSearchDataFromFile(Gson gson, int condition) throws FileNotFoundException {
-        searches = gson.fromJson(new JsonReader(new FileReader(JSON)), Search[].class);
+    public static Search getSearchDataFromFile(Gson gson, int condition, Properties paths) throws IOException {
+        Search[] searches = gson.fromJson(new JsonReader(new FileReader(paths.getProperty("JSON"))), Search[].class);
         return searches[condition];
     }
 
-    public static List<String> getAllUsernames(String response) {
-        Pattern pattern = Pattern.compile("\"username\": \"[A-z]+\"");
-        Matcher matcher = pattern.matcher(response);
-        List<String> list = new ArrayList<>();
-        while (matcher.find())
-            list.add(matcher.group());
-        list = list.stream().map(s -> s.replaceAll("\"username\": ", "")).map(s -> s.replaceAll("\"", "")).collect(Collectors.toList());
-        return list;
+    public RequiredValues parseResponseToClass(Gson gson, Search search) throws IOException, URISyntaxException {
+        return gson.fromJson(HttpRequestSteps.setHttpResponse(gson, search), RequiredValues.class);
     }
 
-    public static boolean fullCheck(List<String> list, String check) {
-        Pattern pattern = Pattern.compile(String.format("^%s$",check));
-        for (String x : list) {
-            if (!pattern.matcher(x).matches())
-                return false;
-        }
-        return true;
+    public RequiredValues getTestCondition(Gson gson, Properties paths, String condition) throws FileNotFoundException {
+        return gson.fromJson(new JsonReader(new FileReader(paths.getProperty(condition))), RequiredValues.class);
     }
-    public static boolean partialCheck(List<String> list, String check) {
-        Pattern pattern = Pattern.compile(String.format(".*%s.*",check));
-        for (String x : list) {
-            if (!pattern.matcher(x).matches())
-                return false;
-        }
-        return true;
-    }
+
 }
--- src/Dmitry_Sankovsky_project/src/test/java/tests/webservice/WebServiceTest.java
+++ src/Dmitry_Sankovsky_project/src/test/java/tests/webservice/WebServiceTest.java
@@ -4,62 +4,66 @@ import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import properties.PropertyPath;
+import steps.BaseSteps;
 import steps.userWebService.GetDataSteps;
-import steps.userWebService.HttpRequestSteps;
+import utills.RequiredValues;
 import utills.Search;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.Properties;
 
 public class WebServiceTest {
-
+    static GetDataSteps getDataSteps;
     static Gson gson;
+    static Properties paths;
 
     @BeforeClass
-    public static void preCondition() {
+    public static void preCondition() throws IOException {
         gson = new Gson();
+        getDataSteps = new GetDataSteps();
+        paths = BaseSteps.getProperties(PropertyPath.WEB_SERVICE_CONDITIONS);
     }
 
     @Test
     public void allUsersTest() throws IOException, URISyntaxException {
-        Search search = GetDataSteps.getSearchDataFromFile(gson, 0);
-        String response = HttpRequestSteps.setHttpResponse(gson, search);
-        List<String> list = GetDataSteps.getAllUsernames(response);
-        list.forEach(System.out::println);
-        Assert.assertEquals(6, list.size());
+        Search search = getDataSteps.getSearchDataFromFile(gson, 0, paths);
+        RequiredValues result = getDataSteps.parseResponseToClass(gson,search);
+        RequiredValues condition = getDataSteps.getTestCondition(gson,paths,"ALL_USERS");
+        Assert.assertEquals(condition, result);
     }
+
     @Test
     public void partialShortTest() throws IOException, URISyntaxException {
-        Search search = GetDataSteps.getSearchDataFromFile(gson, 1);
-        String response = HttpRequestSteps.setHttpResponse(gson, search);
-        List<String> list = GetDataSteps.getAllUsernames(response);
-        list.forEach(System.out::println);
-        Assert.assertTrue(GetDataSteps.partialCheck(list,"a"));
+        Search search = getDataSteps.getSearchDataFromFile(gson, 1, paths);
+        RequiredValues result = getDataSteps.parseResponseToClass(gson,search);
+        RequiredValues condition = getDataSteps.getTestCondition(gson,paths,"PARTIAL_SHORT");
+        Assert.assertEquals(condition.hashCode(), result.hashCode());
     }
+
     @Test
     public void fullShortTest() throws IOException, URISyntaxException {
-        Search search = GetDataSteps.getSearchDataFromFile(gson, 2);
-        String response = HttpRequestSteps.setHttpResponse(gson, search);
-        List<String> list = GetDataSteps.getAllUsernames(response);
-        list.forEach(System.out::println);
-        Assert.assertTrue(GetDataSteps.fullCheck(list,"a"));
+        Search search = getDataSteps.getSearchDataFromFile(gson, 2, paths);
+        RequiredValues result = getDataSteps.parseResponseToClass(gson,search);
+        RequiredValues condition = getDataSteps.getTestCondition(gson,paths,"FULL_SHORT");
+        Assert.assertEquals(condition, result);
     }
+
     @Test
     public void partialLongTest() throws IOException, URISyntaxException {
-        Search search = GetDataSteps.getSearchDataFromFile(gson, 3);
-        String response = HttpRequestSteps.setHttpResponse(gson, search);
-        List<String> list = GetDataSteps.getAllUsernames(response);
-        list.forEach(System.out::println);
-        Assert.assertTrue(GetDataSteps.partialCheck(list,"al"));
+        Search search = getDataSteps.getSearchDataFromFile(gson, 3, paths);
+        RequiredValues result = getDataSteps.parseResponseToClass(gson,search);
+        RequiredValues condition = getDataSteps.getTestCondition(gson,paths,"PARTIAL_LONG");
+        Assert.assertEquals(condition, result);
     }
+
     @Test
     public void fullLongTest() throws IOException, URISyntaxException {
-        Search search = GetDataSteps.getSearchDataFromFile(gson, 4);
-        String response = HttpRequestSteps.setHttpResponse(gson, search);
-        List<String> list = GetDataSteps.getAllUsernames(response);
-        list.forEach(System.out::println);
-        Assert.assertTrue(GetDataSteps.fullCheck(list,"saldo"));
+        Search search = getDataSteps.getSearchDataFromFile(gson, 4, paths);
+        RequiredValues result = getDataSteps.parseResponseToClass(gson,search);
+        RequiredValues condition = getDataSteps.getTestCondition(gson,paths,"FULL_LONG");
+        Assert.assertEquals(condition, result);
     }
 
 }
